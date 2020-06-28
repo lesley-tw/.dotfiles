7efc4f84ccad71ec17ba04eefbd96fe4b1302e60
lesley-tw <lesleyl.tw@gmail.com>
2020-06-28 16:15:23 +0800

    chore: remove unused conf

--- ZSH_CONFIG/package.zsh
+++ ZSH_CONFIG/package.zsh
@@ -1,12 +1,6 @@
 # vivid
 export LS_COLORS="$(vivid generate molokai)"
 
-# gvm
-[[ -s "$HOME/.gvm/scripts/gvm" ]] && source "$HOME/.gvm/scripts/gvm"
-
-# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
 __conda_setup="$('/Users/lesley_lin/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"

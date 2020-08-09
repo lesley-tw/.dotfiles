827f743cb82286582074a10b471b84d5e48b9a90
kirintwn <cy91244@gmail.com>
2020-08-09 21:02:38 +0800

    fix(zsh): update fzf-tab command for new version

--- .config/zsh/misc.zsh
+++ .config/zsh/misc.zsh
@@ -49,14 +49,15 @@ zstyle ':fzf-tab:complete:_zlua:*' query-string input
 # Reference: https://github.com/Aloxaf/fzf-tab#command
 FZF_TAB_COMMAND=(
   fzf
-  --ansi
-  --expect='$continuous_trigger' # For continuous completion
+  --ansi   # Enable ANSI color support, necessary for showing groups
+  --expect='$continuous_trigger,$print_query' # For continuous completion and print query
   '--color=hl:$(( $#headers == 0 ? 108 : 255 ))'
   --nth=2,3 --delimiter='\x00'  # Don't search prefix
   --layout=reverse --height='${FZF_TMUX_HEIGHT:=75%}'
-  --tiebreak=begin -m --cycle
-  '--query=$query'
-  '--header-lines=$#headers'
+  --tiebreak=begin -m --bind=ctrl-space:toggle --cycle
+  '--query=$query'   # $query will be expanded to query string at runtime.
+  '--header-lines=$#headers' # $#headers will be expanded to lines of headers at runtime
+  --print-query
 )
 zstyle ":fzf-tab:*" command $FZF_TAB_COMMAND
 

d0776852d8bb6726306c00edb91476f06c4f3499
lesley-tw <lesleyl.tw@gmail.com>
2021-12-01 17:12:54 +0800

    fix(zinit): fix zinit repo path, remove exa, rg...

--- .config/zsh/zinit.zsh
+++ .config/zsh/zinit.zsh
@@ -1,8 +1,8 @@
 ### Zinit's installer
 if [[ ! -f "$ZINITDIR"/bin/zinit.zsh ]]; then
-  print -P "%F{33}▓▒░ %F{220}Installing %F{33}DHARMA%F{220} Initiative Plugin Manager (%F{33}zdharma/zinit%F{220})…%f"
+  print -P "%F{33}▓▒░ %F{220}Installing %F{33}ZDHARMA-CONTINUUM%F{220} Initiative Plugin Manager (%F{33}zdharma-continuum/zinit%F{220})…%f"
   command mkdir -p "$ZINITDIR" && command chmod g-rwX "$ZINITDIR"
-  command git clone https://github.com/zdharma/zinit "$ZINITDIR"/bin && \
+  command git clone https://github.com/zdharma-continuum/zinit "$ZINITDIR"/bin && \
     print -P "%F{33}▓▒░ %F{34}Installation successful.%f%b" || \
     print -P "%F{160}▓▒░ The clone has failed.%f%b"
 fi
@@ -28,51 +28,30 @@ zinit snippet OMZ::plugins/git/git.plugin.zsh
 zinit snippet OMZ::plugins/sudo/sudo.plugin.zsh
 zinit snippet OMZ::plugins/aws/aws.plugin.zsh
 zinit snippet OMZ::plugins/kubectl/kubectl.plugin.zsh
+zinit snippet OMZ::plugins/helm/helm.plugin.zsh
 zinit ice svn
 zinit snippet OMZ::plugins/extract
 
 # zsh-completions
 zinit ice blockf
 zinit light zsh-users/zsh-completions
-
-# bat
-zinit ice as"program" from"gh-r" mv"bat* -> bat" pick"bat/bat"
-zinit light sharkdp/bat
-
-# exa
-zinit ice from"gh-r" as"program" mv"exa* -> exa"
-zinit light ogham/exa
+zinit ice as"completion"
+zinit snippet "https://github.com/docker/cli/blob/master/contrib/completion/zsh/_docker"
 zinit ice as"completion"
 zinit snippet "https://github.com/ogham/exa/blob/master/completions/zsh/_exa"
 
-# jq
-zinit ice from"gh-r" as"program" mv"jq* -> jq"
-zinit light stedolan/jq
-
-# fd
-zinit ice as"program" from"gh-r" mv"fd* -> fd" pick"fd/fd"
-zinit light sharkdp/fd
-
-# rg
-zinit ice as"program" from"gh-r" mv"ripgrep* -> ripgrep" pick"ripgrep/rg"
-zinit light BurntSushi/ripgrep
-
-# fzf-bin
-zinit ice from"gh-r" as"program"
-zinit light junegunn/fzf-bin
-zinit snippet "https://github.com/junegunn/fzf/blob/master/shell/completion.zsh"
+# fzf key bindings - https://github.com/junegunn/fzf#key-bindings-for-command-line
 zinit snippet "https://github.com/junegunn/fzf/blob/master/shell/key-bindings.zsh"
 
-# delta
-zinit ice as"program" from"gh-r" mv"delta* -> delta" pick"delta/delta"
-zinit light dandavison/delta
+# z.lua
+zinit light skywind3000/z.lua
 
 # fzf-tab
 zinit light Aloxaf/fzf-tab
 
 # fast-syntax-highlighting
 zinit ice wait lucid atinit"ZINIT[COMPINIT_OPTS]=-C; zpcompinit; zpcdreplay"
-zinit light zdharma/fast-syntax-highlighting
+zinit light zdharma-continuum/fast-syntax-highlighting
 
 # zsh-autosuggestions
 zinit ice wait lucid atload"!_zsh_autosuggest_start"

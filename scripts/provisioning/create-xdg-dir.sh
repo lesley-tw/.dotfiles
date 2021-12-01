6c48eba257bde0c2be38422900732e090e9cfe67
lesley-tw <lesleyl.tw@gmail.com>
2021-12-01 23:55:27 +0800

    feat(scripts): add xdg path

--- scripts/provisioning/create-xdg-dir.sh
+++ scripts/provisioning/create-xdg-dir.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
-mkdir -p "$XDG_CONFIG_HOME/{aws,bat,docker,k9s,nvim,zsh}"
-mkdir -p "$XDG_DATA_HOME/{docker-machine,go,node,nvim,zinit,zlua,zsh}"
-mkdir -p "$XDG_CACHE_HOME/zcompdump"
+XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
+XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
+
+mkdir -p "$XDG_CONFIG_HOME"/{aws,bat,docker,k9s,nvim,tealdeer,zsh}
+mkdir -p "$XDG_DATA_HOME"/{docker-machine,go,node,nvim,zinit,zlua,zsh}
+mkdir -p "$XDG_CACHE_HOME"/{tealdeer,zcompdump}
+
+touch "$XDG_CONFIG_HOME"/aws/{config,credentials}
+touch "$XDG_DATA_HOME/zlua/.zlua"
+mkdir -p "$HOME/.local/bin"

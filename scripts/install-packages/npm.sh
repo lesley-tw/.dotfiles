274e51b2952085d494637f022e4e59f82aca9dd3
lesley-tw <lesleyl.tw@gmail.com>
2021-12-01 17:14:43 +0800

    refactor(multi): refactor packages

--- scripts/install-packages/npm.sh
+++ scripts/install-packages/npm.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
 
-# Prerequisites:
-# * npm - install from asdf
-
 npm install -g npm
-
-npm install -g pm2
-npm install -g npm-check
+npm install -g yarn
+npm install -g npm-check-updates
 npm install -g fx
 npm install -g prettier
 npm install -g neovim
-
--- scripts/install-packages/pip.sh
+++ scripts/install-packages/pip.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 
-# Prerequisites:
-# * pip - install from asdf
-
 pip install --upgrade pip
 
 pip install --user --upgrade awscli
 pip install --user --upgrade glances
 pip install --user --upgrade neovim
-

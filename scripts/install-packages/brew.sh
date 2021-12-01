ec5dc429808f331c0d6a10de502704a89ad11fac
lesley-tw <lesleyl.tw@gmail.com>
2021-12-01 17:13:25 +0800

    feat(brew): add more packages

--- scripts/install-packages/brew.sh
+++ scripts/install-packages/brew.sh
@@ -1,30 +1,63 @@
 #!/usr/bin/env bash
 
 # Prerequisites:
-# * Xcode - from Apple App Store
-# * Homebrew - /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
+# * Homebrew - /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew update
 brew upgrade
 
-brew install zsh
-# To use zsh as default shell:
-# *	sudo sh -c "echo $(which zsh) >> /etc/shells" 
-# *	chsh -s $(which zsh)
-
+brew install git
+brew install subversion
 brew install less
 brew install gnupg
+brew install wget
 brew install neofetch
-brew install aws-iam-authenticator
-brew install kubectl
-brew install derailed/k9s/k9s
-brew install tealdeer
 brew install neovim
 brew install --HEAD universal-ctags/universal-ctags/universal-ctags
+brew install fnm
+brew install python
+brew install bpython
+brew install lua
+brew install bat
+brew install exa
+brew install fd
+brew install ripgrep
+brew install fzf
+brew install git-delta
+brew install tmux
+
+brew install awscli
+brew install kubernetes-cli
+brew install k9s
+brew install kubefwd
+brew install helm
+brew install tealdeer
+brew install croc
+brew install cfn-lint
+brew install mysql-client
+brew install shellcheck
+brew install hadolint
+brew install yabai --head
+brew install skhd
 
 brew tap homebrew/cask-fonts
-
-brew cask install iterm2
-brew cask install font-sourcecodepro-nerd-font
-
+brew install --cask font-fira-code-nerd-font
+brew install --cask iterm2
+brew install --cask google-chrome
+brew install --cask visual-studio-code
+brew install --cask docker
+brew install --cask spotify
+brew install --cask telegram
+brew install --cask alacritty
+brew install --cask balance-lock
+brew install --cask scroll-reverser
+brew install --cask karabiner-elements
+brew install --cask bettertouchtool
+brew install --cask yabai
+brew install --cask iina
+brew install --cask zoom
+brew install --cask caffeine
+brew install --cask notion
+brew install --cask sublime-text
+brew install --cask itsycal
+brew install --cask appcleaner
 brew cleanup
